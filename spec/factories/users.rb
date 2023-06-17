12dfa0e97bd739579b73093e144f133b93d78315
1682123 <123kato.izumi@gmail.com>
2023-06-17 15:27:34 +0900

    Userモデルのテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :user do
+    japanese_user = Gimei.name
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password ('1a' + Faker::Internet.password(min_length: 6))
+    password {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name {Faker::Name.last_name}
-    first_name {Faker::Name.first_name}
-    last_name_kana {Faker::Name.last_name}
-    first_name_kana {Faker::Name.first_name}
+    last_name { japanese_user.first.kanji }
+    first_name { japanese_user.last.kanji }
+    last_name_kana { japanese_user.first.katakana }
+    first_name_kana { japanese_user.last.katakana }
     birthday {Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end
